6e3060d9875074819ba413548ce76dd2485b1f5b
Zvezdilin <artem@intervolga.ru>
2020-08-06 13:48:29 +0400

    base solution

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const config = require('./package.json');
+const httpProxy = require('express-http-proxy');
+
+const port = process.env.PORT || config.prot;
+const proxy = process.env.PROXY || config.proxy;
+const login = process.env.LOGIN || config.proxy;
+const password = process.env.PASSWORD || config.password;
+
+app.use(cors());
+app.use(httpProxy(`${proxy}`, {
+  https: true,
+  proxyReqOptDecorator: function(proxyReqOpts) {
+    proxyReqOpts['auth'] = `${login}:${password}`;
+    proxyReqOpts['Authorization'] = `Basic ${Buffer.from(`${login}:${password}`).toString('base64')}`;
+    proxyReqOpts['Proxy-Authorization'] = `Basic ${Buffer.from(`${login}:${password}`).toString('base64')}`;
+    return proxyReqOpts;
+  }
+}));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+});
