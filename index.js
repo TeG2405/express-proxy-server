a1023461fbf63d472124b29c5e730236b9d619c3
ZvezdilinAK <artem@intervolga.ru>
2021-02-16 14:53:05 +0300

    Игнорируем не подписанные сертификаты

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.use(cors());
 app.use(httpProxy(`${host}`, {
   https: true,
   proxyReqOptDecorator: function(proxyReqOpts) {
+    proxyReqOpts.rejectUnauthorized = false;
     return proxyReqOpts;
   },
   userResHeaderDecorator(headers, userReq, userRes, proxyReq, proxyRes) {
